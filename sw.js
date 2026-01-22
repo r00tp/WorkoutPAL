2eae5c4adc3f3a86d8dc25470fb8a17a4ae7b75b
pierrebarcella <pierre.barcella@sosoxygene.com>
2026-01-22 14:04:57 +0100

    1.6.2
    **ADD**
    - Ajout de la version en footer du menu
    
    **FIX**
    - Exercice ne bascule pas au suivant lorsque le timer est accéléré sur la dernière série
    - Pas de timer repos sur la dernière série d'un exercice (bascule immédiat sur suivant)
    - Erreur lors de la suppression d'une séance
    - Mise à jour du cache dans sw.js

--- sw.js
+++ sw.js
@@ -1,21 +1,38 @@
-const CACHE_NAME = 'workout-tracker-v1';
+const CACHE_NAME = 'workoutpal-1.6.2';
 const urlsToCache = [
   './',
-  './workout-tracker.html',
-  './manifest.json'
+  './index.html',
+  './workouts-data.json',
+  './manifest.json',
+  'https://fonts.googleapis.com/icon?family=Material+Icons'
 ];
 
 self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then((cache) => {
+        return cache.addAll(urlsToCache).catch(err => {
+          console.error('Erreur de mise en cache:', err);
+        });
+      })
   );
+  self.skipWaiting();
 });
 
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => response || fetch(event.request))
+      .then((response) => {
+        return response || fetch(event.request).then(fetchResponse => {
+          return caches.open(CACHE_NAME).then(cache => {
+            cache.put(event.request, fetchResponse.clone());
+            return fetchResponse;
+          });
+        });
+      })
+      .catch(() => {
+        console.log('Ressource non disponible hors ligne');
+      })
   );
 });
 
@@ -31,4 +48,5 @@ self.addEventListener('activate', (event) => {
       );
     })
   );
+  self.clients.claim();
 });
